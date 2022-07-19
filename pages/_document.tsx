45e5069470f427187c1d9e87c2f7f13a074c5eaa
sonangrai <shonahangrae@gmail.com>
2022-07-19 17:16:48 +0545

    added favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -37,6 +37,7 @@ export default class MyDocument extends Document {
             name="viewport"
             content="initial-scale=1.0, width=device-width"
           />
+          <link rel="icon" type="image/x-icon" href="/lukuku.png"></link>
           {/* Add stuff you want in your header from here */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
