d2c76b29603192f780d44dc1c4ab3705b5824266
sonangrai <shonahangrae@gmail.com>
2022-07-19 16:57:35 +0545

    added styled theme

---
+++ styled/Theme.ts
@@ -0,0 +1,16 @@
+/**
+ * This file can contain all the theme realted variables like colors, fontSize, fontWeight etc for the styled-components
+ */
+export const theme = {
+  //Colors
+  primaryColor: "#C40000",
+  black: "#000",
+  grey: "#7F7F7F",
+  lightGrey: "#EBECED",
+
+  //Font Weights
+  fntWgtRegular: "400",
+  fntWgtMedium: "500",
+  fntWgtSBold: "600",
+  fntWgtBold: "700",
+};
