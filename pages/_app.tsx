5099d5d9cc28088e6520040b2a0b8a4e27f6ce34
sonangrai <shonahangrae@gmail.com>
2022-07-20 11:01:16 +0545

    added page loader bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,17 @@
 import GlobalStyle from "../styled/GlobalStyle";
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
+
+const progress = new ProgressBar({
+  size: 4,
+  color: "#4cba4d",
+  className: "bar-of-progress",
+  delay: 100,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 function App({ Component, pageProps }) {
   return (
