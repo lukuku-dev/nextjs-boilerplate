6f2806ffc34bec2c238d3a9cf2b0d901d5de47a1
sonangrai <shonahangrae@gmail.com>
2023-05-25 13:26:51 +0545

    updated breakpoint

--- styled/Breakpoint.ts
+++ styled/Breakpoint.ts
@@ -2,22 +2,12 @@
  * This js contains the breakpoint variables for responsive stylings
  */
 const size = {
-  mobileS: "320px",
-  mobileM: "375px",
-  mobileL: "430px",
   tablet: "768px",
-  laptop: "1024px",
-  laptopL: "1440px",
-  desktop: "2560px",
+  desktop: "1024px",
 };
 
 export const device = {
-  mobileS: `(max-width: ${size.mobileS})`,
-  mobileM: `(max-width: ${size.mobileM})`,
-  mobileL: `(max-width: ${size.mobileL})`,
   tablet: `(max-width: ${size.tablet})`,
-  laptop: `(max-width: ${size.laptop})`,
-  laptopL: `(max-width: ${size.laptopL})`,
   desktop: `(max-width: ${size.desktop})`,
 };
 
