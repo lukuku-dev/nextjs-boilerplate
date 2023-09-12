0845b476dedaa76396b9ad78c4f100e80f54cd59
sonangrai <shonahangrae@gmail.com>
2023-09-12 17:17:13 +0545

    dependency list

--- components/home/Greeting.tsx
+++ components/home/Greeting.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import styled from "styled-components";
+import packages from "package.json";
+
 interface Props {
   message: string;
 }
@@ -11,7 +13,21 @@ function Greeting({ message }: Props) {
         <CompanyLogo>
           <Image src="/lukuku.png" width={50} height={50} alt="Alt Text" />
         </CompanyLogo>
-        {message}
+        <h2>{message}</h2>
+
+        <Title>Dependencies</Title>
+        <List>
+          {Object.entries(packages.dependencies).map((pack, key) => (
+            <li key={key}>{pack}</li>
+          ))}
+        </List>
+
+        <Title>Dev Dependencies</Title>
+        <List>
+          {Object.entries(packages.devDependencies).map((pack, key) => (
+            <li key={key}>{pack}</li>
+          ))}
+        </List>
       </div>
     </GreetingSection>
   );
@@ -36,6 +52,21 @@ const CompanyLogo = styled.div`
   width: 100px;
   height: 100px;
   text-align: center;
+`;
+
+const Title = styled.h3`
+  color: #777;
+  margin: 0;
+`;
+
+const List = styled.ul`
+  li {
+    list-style: none;
+    font-size: 16px;
+    color: #777;
+    text-align: center;
+    line-height: 24px;
+  }
 `;
 
 export default Greeting;
