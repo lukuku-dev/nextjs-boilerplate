07ba27eddafed488fdd7ce8c7ef6ebda04c3c0c6
sonangrai <shonahangrae@gmail.com>
2023-12-13 16:47:12 +0545

    feat(calculated vh): calculated vh css variable is now added

--- utils/registery.tsx
+++ utils/registery.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useServerInsertedHTML } from "next/navigation";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ServerStyleSheet, StyleSheetManager } from "styled-components";
 import GlobalStyle from "styled/GlobalStyle";
 
@@ -19,6 +19,25 @@ export default function StyledComponentsRegistry({
     styledComponentsStyleSheet.instance.clearTag();
     return <>{styles}</>;
   });
+
+  // Calculating real VH and creating css variable
+  useEffect(() => {
+    function setRealViewportHeight() {
+      let vh = window.innerHeight;
+      document.documentElement.style.setProperty("--real-vh", `${vh}px`);
+    }
+
+    // Set the initial height
+    setRealViewportHeight();
+
+    // Update the height whenever the window is resized
+    window.addEventListener("resize", setRealViewportHeight);
+
+    // Cleanup listener on component unmount
+    return () => {
+      window.removeEventListener("resize", setRealViewportHeight);
+    };
+  }, []); // The empty dependency array means this useEffect runs once after the component mounts and then runs
 
   if (typeof window !== "undefined") return <>{children}</>;
 
