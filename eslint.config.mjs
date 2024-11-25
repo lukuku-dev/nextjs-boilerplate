9d504856d1711b618d90146e5e5f2f1327cdec2d
sonangrai <shonahangrae@gmail.com>
2024-11-25 10:58:22 +0545

    feat: upgraded packages

---
+++ eslint.config.mjs
@@ -0,0 +1,106 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import importHelpers from "eslint-plugin-import-helpers";
+import react from "eslint-plugin-react";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: [
+      "**/node_modules/*",
+      "**/out/*",
+      "**/.next/*",
+      "**/coverage",
+      "src/styles/globals.css",
+    ],
+  },
+  ...compat.extends(
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier"
+  ),
+  {
+    plugins: {
+      react,
+      "@typescript-eslint": typescriptEslint,
+      "import-helpers": importHelpers,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 11,
+      sourceType: "module",
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+
+    rules: {
+      "newline-before-return": 2,
+      "react/prop-types": 0,
+      "react/react-in-jsx-scope": 0,
+      "import-helpers/order-imports": [
+        2,
+        {
+          newlinesBetween: "always",
+
+          groups: [
+            ["/^next/", "module"],
+            "/^@/styles/",
+            "/^@/components/",
+            "/^@/lib/",
+            ["parent", "sibling", "index"],
+          ],
+
+          alphabetize: {
+            order: "asc",
+            ignoreCase: true,
+          },
+        },
+      ],
+
+      "@typescript-eslint/no-unused-vars": [
+        2,
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+
+      "no-console": [
+        2,
+        {
+          allow: ["warn", "error"],
+        },
+      ],
+    },
+  },
+];
